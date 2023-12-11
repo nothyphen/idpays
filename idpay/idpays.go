ca3a4c707d09ee64c5f942315e574f8c6d50f87c
fakehyphen <fakehyphen@gmail.com>
2023-12-11 16:26:19 +0330

    change the file and made the user only have to enter apikey once

--- idpay/idpays.go
+++ idpay/idpays.go
@@ -1,13 +1,17 @@
 package idpay
 
-type IdPays interface {
-	Payment(SANDBOX string, API_KEY string, order_id int, amount int, name string, phone string, mail string, desc string, callback string) (string, string, error)
-	Verify(id string, order_id string) (interface{}, error)
+import "github.com/nothyphen/idpays/serializers"
+
+type idPays interface {
+	Payment(SANDBOX string, order_id int, amount int, name string, phone string, mail string, desc string, callback string) (string, string, error)
+	Verify(SANDBOX string, id string, order_id string) (*serializers.VerifyRequest, error)
 	Inquiry(id string, order_id string) (interface{}, error)
 }
 
-type idpays struct {}
+type idpays struct {
+	apikey string
+}
 
-func NewIdPays() IdPays{
-	return &idpays{}
+func NewIdPays(API_KEY string) *idpays{
+	return &idpays{apikey: API_KEY}
 }
