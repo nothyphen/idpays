1b9e76a170652ec610c96c26fcc884b3753db444
fakehyphen <fakehyphen@gmail.com>
2023-12-11 16:30:20 +0330

    create verify transaction function and complete

--- idpay/verify.go
+++ idpay/verify.go
@@ -1,5 +1,57 @@
 package idpay
 
-func (i * idpays) Verify(id string, order_id string) (interface{}, error) {
-	return "ok", nil
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/nothyphen/idpays/serializers"
+)
+
+func (i * idpays) Verify(SANDBOX string, id string, order_id string) (*serializers.VerifyRequest, error) {
+	verify_url := "https://api.idpay.ir/v1.1/payment/verify"
+	
+	data := map[string]string{
+		"id" : id,
+		"order_id" : order_id,
+	}
+
+	payload, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	request, err := http.NewRequest("POST", verify_url, bytes.NewBuffer(payload))
+	if err != nil {
+		panic(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("X-API-KEY", i.apikey)
+	request.Header.Add("X-SANDBOX", SANDBOX)
+
+	client := http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		msg, _ := ioutil.ReadAll(response.Body)
+		buf := map[string]interface{}{}
+		err = json.Unmarshal(msg, &buf)
+		fmt.Println(buf["error_code"], buf["error_message"])
+		return nil, err
+	}
+
+	bindRequest := &serializers.VerifyRequest{}
+	derr := json.NewDecoder(response.Body).Decode(bindRequest)
+	if derr != nil {
+		panic(derr)
+	}
+	return bindRequest, nil
 }
