fe073f83b7dd0f036c26019df10d4a5b955a02c3
fakehyphen <fakehyphen@gmail.com>
2023-12-11 16:31:06 +0330

    test functions and they work perfectly

--- test/test.go
+++ test/test.go
@@ -7,7 +7,13 @@ import (
 )
 
 func main() {
-	idpay := idpay.NewIdPays()
-	id, link, _ := idpay.Payment("1", "d50a77ba-124a-40ef-9e7b-b326a721921b", 101, 10000, "porya", "09111111111", "pourya@ikernel.ir", "hichi", "http://ikernel.ir/callback")
+	idpay := idpay.NewIdPays("d50a77ba-124a-40ef-9e7b-b326a721921b")
+	
+	id, link, _ := idpay.Payment("1", 101, 10000, "porya", "09111111111", "pourya@ikernel.ir", "hichi", "http://ikernel.ir/callback")
 	fmt.Println(id, link)
+
+	_, err := idpay.Verify("1", "d2e353189823079e1e4181772cff5292", "101")
+	if err != nil {
+		panic(err)
+	}
 }
